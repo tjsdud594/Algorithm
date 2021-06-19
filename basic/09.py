1cc80e47fc2bbdeb12e1832e26a13b034f382958
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-06-20 00:23:18 +0900

    number 9

---
+++ basic/09.py
@@ -0,0 +1,35 @@
+'''
+문제 설명
+이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+제한 조건
+n과 m은 각각 1000 이하인 자연수입니다.
+
+예시
+입력
+
+5 3
+출력
+
+*****
+*****
+*****
+'''
+
+# map(변환 함수, 순회 가능한 데이터)
+# strip() : 양쪽 공백 모두 삭제
+
+
+# solution 1
+a, b = map(int, input().strip().split(' '))
+for i in range(1, b+1):
+    print(a*"*")
+
+# solution 2
+a, b = map(int, input().strip().split(' '))
+answer = ('*'*a +'\n')*b
+print(answer)
+
+
+
