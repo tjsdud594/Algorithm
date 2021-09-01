f31fe51cf0bfd3bafe82d0e676464faeb1f53398
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-09-01 14:20:32 +0900

    비완주자

--- basic/24.py
+++ basic/24.py
@@ -64,6 +64,7 @@ def solution(participant, completion):
     for com in completion:
         for part in participant:
             if part != com:
+                # pop() : 리스트의 처음부터 훑기 때문에 효율성에서 문제
                 participant.pop(participant.index(com))
                 break
 
