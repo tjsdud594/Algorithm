bb44108b175223e8d3001901f8a5ca2c1e8542e2
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-01-16 19:28:01 +0900

    fix : 해결완료

--- basic/Boat.py
+++ basic/Boat.py
@@ -3,95 +3,76 @@
 ###############################
 #           푸는중            #
 ###############################
-
+import time
 
 # 생각을 잘못했음 최솟값끼리 더하는게 아니라 연속수합 구하듯이 최솟값+최댓값이 limit 아래로 오도록 조합을 짰어야한다.
 def solution(people, limit):
-    # answer = 0
-    # 한 번에 최대 2명씩!!!
-    # list sort
-    sorted_people = sorted(people)
-    print(sorted_people)    
-    
-    # 작은값부터 정하기(index while문)
-    index = 0
-    total_weight = 0
-    boat_people_cnt = 0  
-    while index<len(people):
-        # index 0부터 시작해서 
-        # 가장 index==0은 무게+1, people_cnt+1
-        if index==0:
-            answer=1  # 첫사람을 태울보트
-            boat_people_cnt+=1  # 첫번째 사람이 탔다
-            total_weight+=sorted_people[index]  # 첫번째 사람의 무게
-            index+=1
-            print(f"첫사람")
-            print(f"answer : {answer}")
-            print(f"boat_people_cnt : {boat_people_cnt}")
-            print(f"total_weight : {total_weight}")
-            print("===========================================================================")
+    answer = 0
+    total_people = len(people)
 
+    # 사람이 2명 이하일 때
+    if total_people<=2:
+        if sum(people)>limit:
+            return total_people
         else:
-            # 만약 지금 보트를 타려는 사람이 limit 절반을 넘는 무게를 가졌다면 이후의 사람들은 모두 보트1대씩 타게된다.
-            if sorted_people[index]>(limit//2)+1 and (total_weight+sorted_people[index])>limit:
-                answer+=(len(sorted_people)-index)
-                print(f"limit 절반초과")
-                print(f"answer : {answer}")
-                print(f"boat_people_cnt : {boat_people_cnt}")
-                print(f"total_weight : {total_weight}")
-                print("===========================================================================")
+            return 1
+    
+    # 사람이 3명 이상일 때
+    else:
+        start = time.time()
+        sorted_people = sorted(people)
+        end = time.time()
+        print(f"sort시간 : {end - start:.5f} sec")
+
+        # print(sorted_people)
+        least_weight = sorted_people[0]
+        max_index=0
+
+        start = time.time()
+        for i in range(total_people-1, 0, -1):
+            if least_weight+sorted_people[i]<=limit:
+                max_index=i
                 break
+        end = time.time()
+        print(f"같이 탈 수 있는 최저무게 찾기 : {end - start:.5f} sec")
 
-            else:
-
-                total_weight+=sorted_people[index]
-
-                # 무게를 초과했을경우 -> 새보트
-                # 보트인원(2)을 초과했을경우 -> 새보트
-                # 무게는 초과하지 않았지만 보트인원을 초과했을경우(3이상) -> 새보트
-                print("현재 보트 상태")
-                print(f"인원수 : {answer}")
-                print(f"보트무게 : {total_weight}")
-                print(f"탑승자수 : {boat_people_cnt}")
-                print("===========================================================================")
-
-                if (total_weight>limit) or (boat_people_cnt>2):
-                    answer+=1 # 보트개수+1(현재사람이 탈 보트)
-                    boat_people_cnt=1 # 인원수 초기화
-                    total_weight=sorted_people[index] # 무게 조기화
-                    # index+=1 # 다음사람으로 넘어감
-                    print(f"초과해서 다음사람으로 넘어감")
-                    print(f"answer : {answer}")
-                    print(f"boat_people_cnt : {boat_people_cnt}")
-                    print(f"total_weight : {total_weight}")
-                    print("===========================================================================")
-
+        if max_index==0:
+            return total_people
+        
+        else:
+            answer+=(total_people-max_index)-1
+            # print(f"초기 1인 탑승 : {answer}명")
+            start_index=0
+            start = time.time()
+            while start_index<=max_index:
+                if sorted_people[start_index]+sorted_people[max_index]>limit:
+                    # print(f"무게초과 {sorted_people[start_index]}+{sorted_people[max_index]}")
+                    max_index-=1
+                    answer+=1
+                    continue
                 else:
-                    answer+=0
-                    boat_people_cnt+=1
-                    total_weight=total_weight
-                    print(f"초과안해서 그대로 추가됨")
-                    print(f"answer : {answer}")
-                    print(f"boat_people_cnt : {boat_people_cnt}")
-                    print(f"total_weight : {total_weight}")
-                    print("===========================================================================")
-            
-            index+=1
-
-            # 무게도 초과하지 않고 보트인원도 초과하지 않았을경우(0,1) -> 현재 보트에 인원 및 무게추가
-
+                    # print(f"무게미달 {sorted_people[start_index]}+{sorted_people[max_index]}")
+                    start_index+=1
+                    max_index-=1
+                    answer+=1
+                    continue
+            end = time.time()
+            print(f"동반탑승 구분하기 : {end - start:.5f} sec")
         
     return answer
 
 
 if __name__ == "__main__":
     import time
+    import random
 
     start = time.time()
-    people = [40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
-    limit = 130
+    # people = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    # limit = 100
     # people = [41, 42, 43, 44]
     # limit = 200
+    people = [random.randint(40, 240) for _ in range(50000)]
+    limit=240
     print(solution(people, limit))
     end = time.time()
 
