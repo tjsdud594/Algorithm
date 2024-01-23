0eea2bf439f4b4e6b047ffd0982fb3050dda478a
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-01-23 21:45:53 +0900

    fix : 큰수만들기 재풀이 시간초과

--- basic/MakeBigNum.py
+++ basic/MakeBigNum.py
@@ -1,25 +1,36 @@
-'''
-[문제 설명]
-어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+### 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42883
 
-예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 
-이 중 가장 큰 숫자는 94 입니다.
+###############################
+#           풀이 중 - 시간초과          #
+###############################
 
-문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. 
-number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
-
-[제한 조건]
-number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
-k는 1 이상 number의 자릿수 미만인 자연수입니다.
-
-[입출력 예]
-number	        k	return
-"1924"	        2	"94"
-"1231234"	    3	"3234"
-"4177252841"	4	"775841"
-'''
 
 def solution(number, k):
+    answer=''
+    index=0
+    number_list = list(number)
+
+
+    while index<len(number_list)-1 and k>0:
+        # print(number_list)
+        # print(number_list[index], number_list[index+1], k)
+        if number_list[index]<number_list[index+1]:
+            number_list.pop(index)
+            index=0
+            k-=1
+        else:
+            index+=1
+
+    if k>0:
+        number_list = number_list[:-k]
+    
+    
+    return ''.join(number_list)
+
+
+
+# 이전 풀이
+def solution_bf(number, k):
 
     ans = []
     for num in number:
@@ -42,3 +53,16 @@ def solution(number, k):
     if k>0:
         ans = ans[:-k]
     return "".join(ans)
+
+
+if __name__ == "__main__":
+    import time
+    number = "4177252841"
+    k=4
+
+    print("start")
+    start = time.time()
+    print(solution(number, k))
+    end = time.time()
+
+    print(f"{end - start:.5f} sec")
