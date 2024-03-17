38a4993a80064340f907d2465b3e09c996827b28
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-03-17 16:21:33 +0900

    edit : 등굣길 재풀이

--- basic/programmers/WayToSchool.py
+++ basic/programmers/WayToSchool.py
@@ -5,38 +5,38 @@
 ###############################
 
 def solution(m, n, puddles):
-    loads = [[1 for _ in range(m)] for __ in range(n)] 
 
-    if puddles!=[[]]:
-        for [x,y] in puddles:
-            loads[y-1][x-1]=0
+    map = [[1 for _ in range(m)] for __ in range(n)]
+    if puddles!=[]:
+        for puddle in puddles:
+            [x, y] = puddle
+            map[y-1][x-1]=0
 
-    x_num=1
-    y_num=1
-    for i in range(m):
-        if loads[0][i]==0:
-            x_num=0
-        loads[0][i]=x_num
-    for j in range(n):
-        if loads[j][0]==0:
-            y_num=0
-        loads[j][0]=y_num
-    
-    for x in range(1, m):
-        for y in range(1, n):
-            if loads[y][x]==0:
-                continue
-            else:
-                loads[y][x] = loads[y-1][x] + loads[y][x-1]
+    flag=1
+    for x in range(m):
+        if map[0][x]==0:
+            flag=0
+        map[0][x] = flag
 
-    return loads[n-1][m-1]%1000000007
+    flag=1
+    for y in range(n):
+        if map[y][0]==0:
+            flag=0
+        map[y][0]=flag
+
+    for y in range(1, n):
+        for x in range(1, m):
+            if map[y][x]!=0:
+                map[y][x] = map[y-1][x] + map[y][x-1]
+
+    return map[n-1][m-1]%1000000007
 
 if __name__ == "__main__":
     import time
 
-    m=100
-    n=100
-    puddles=[]
+    m=7
+    n=4
+    puddles=[[2, 1], [2, 2], [2, 3], [4, 2], [4, 3], [4, 4], [6, 2], [6, 3]]
 
     start = time.time()
     print(solution(m, n, puddles))
