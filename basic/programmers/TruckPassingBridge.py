1e20c5e97d7d950e9c6eac3616cee0926d2f56b4
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-04-16 20:01:49 +0900

    edit : 재풀이

--- basic/programmers/TruckPassingBridge.py
+++ basic/programmers/TruckPassingBridge.py
@@ -6,34 +6,31 @@
 
 
 def solution(bridge_length, weight, truck_weights):
-    from collections import deque, defaultdict
-    answer=0
-    truck_weights = deque(truck_weights)
-    on_bridge = deque([0]*bridge_length)
+    from collections import deque
+    answer = 0
 
-    now_weight = 0
+    on_bridge = deque([0]*bridge_length)
+    truck_weights = deque(truck_weights)
+    now_bridge = 0
+
     while truck_weights:
         answer+=1
-        now_weight-=on_bridge.popleft()
-
-        # if sum(on_bridge)+truck_weights[0]<=weight:  sum을 쓰면 효율성이 낮아진다!!
-        if now_weight+truck_weights[0]<=weight:
-            now_weight+=truck_weights[0]
+        now_bridge -= on_bridge.popleft()
+        if now_bridge+truck_weights[0]<=weight:
+            now_bridge+=truck_weights[0]
             on_bridge.append(truck_weights.popleft())
         else:
             on_bridge.append(0)
 
-    answer+=len(on_bridge)
-
-    return answer
+    return answer+len(on_bridge)
 
 
 if __name__ == "__main__":
     import time
 
-    bridge_length = 100
-    weight = 100
-    truck_weights = [10,10,10,10,10,10,10,10,10,10]
+    bridge_length = 2
+    weight = 10
+    truck_weights = [7,4,5,6]
 
     start = time.time()
     print(solution(bridge_length, weight, truck_weights))
