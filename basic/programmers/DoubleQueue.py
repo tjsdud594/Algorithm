43aa668595e6f80edfe41d08a6fb0adbba415dae
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-04-18 19:13:00 +0900

    edit : heapq 사용 풀이 추가

--- basic/programmers/DoubleQueue.py
+++ basic/programmers/DoubleQueue.py
@@ -7,6 +7,7 @@
 
 def solution(operations):
     from collections import deque
+
     answer = deque([])
 
     for instance in operations:
@@ -27,14 +28,40 @@ def solution(operations):
     return [answer[0], answer[-1]] if answer!=deque([]) else [0, 0]
 
 
+def solution2(operations):
+    import heapq
+    answer = []
+
+    for instance in operations:
+        op, val = instance.split()
+        heapq.heapify(answer)
+
+        if op=="I":
+            heapq.heappush(answer, int(val))
+        else:
+            if answer==[]:
+                continue
+            elif val=="-1":
+                heapq.heappop(answer)
+            else:
+                heapq._heapify_max(answer)
+                heapq.heappop(answer)
+        # print(f"instance : {instance}, answer : {answer}")
+
+    min_heap = answer[:]
+    heapq.heapify(min_heap)
+    max_heap = answer[:]
+    heapq._heapify_max(max_heap)
+    return [max_heap[0], min_heap[0]] if answer!=[] else [0,0]
+
 
 if __name__ == "__main__":
     import time
 
-    operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+    operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
 
     start = time.time()
-    print(solution(operations))
+    print(solution2(operations))
     end = time.time()
 
     print(f"{end - start:.5f} sec")
