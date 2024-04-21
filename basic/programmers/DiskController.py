23d4747c2634e3ba4b779d9c34f13ca06178f618
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-04-21 21:31:43 +0900

    add : 디스크 컨트롤러

---
+++ basic/programmers/DiskController.py
@@ -0,0 +1,43 @@
+### 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+###############################
+#           풀이완료           #
+###############################
+
+
+import heapq
+
+def solution(jobs):
+    answer = 0
+    now = 0 #현재시간
+    i = 0   #처리개수
+    start = -1 #마지막 완료시간
+    heap = []
+    
+    while i < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap,[job[1],job[0]])
+        
+        if heap:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            answer += now - current[1] #요청으로부터 처리시간
+            i += 1
+        else:
+            now += 1
+            
+    return answer // len(jobs)
+
+
+if __name__ == "__main__":
+    import time
+
+    jobs = [[0, 3], [1, 9], [2, 6]]
+
+    start = time.time()
+    print(solution(jobs))
+    end = time.time()
+
+    print(f"{end - start:.5f} sec")
