86b7b095a1b7284224dddd8a31ec7c2ffb8dae82
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-05-07 20:37:49 +0900

    edit : 오답풀이 완완완

--- basic/programmers/DiskController.py
+++ basic/programmers/DiskController.py
@@ -8,35 +8,39 @@
 
 def solution(jobs):
     import heapq
-    import math
 
-    answer = 0
-    now = 0
+    # 이전 잡이 끝난시간
     start = -1
-    heap = []
-    i = 0
-
-    while i < len(jobs):
-        for job in jobs:
-            # 이전 job 종료시간과 현재 시간 사이에 요청된 job일 경우 대기열에 넣기
-            if start < job[0] <= now:
-                heapq.heappush(heap, [job[1], job[0]])
-                # print(heap)
-
+    # 현재 잡이 끝날 시간
+    now = 0
+    # job 대기열
+    heap = [] 
+    # 완료한 job의 개수
+    clear_job = 0
+    answer = 0
+    
+    while clear_job<len(jobs):
+        # print(f"heap : {heap}, start : {start}, now : {now}")
+        for job_request, job_time in jobs:
+            # 현재 잡이 실행 중일 때 대기열에 들어갈 경우 heap에 넣는다
+            if start<job_request<=now:
+                heapq.heappush(heap, [job_time, job_request])
+            # print(f"heap : {heap}, start : {start}, now : {now}")
+        
+        # heap에 대기중인 잡들을 실행시간이 적은 순으로 실행
         if heap:
-            i+=1
-            spent_time, start_time = heapq.heappop(heap)
+            t, req = heapq.heappop(heap)
             start = now
-            # 각 job의 종료시간을 더하여 이전 job의 종료시간 체크
-            now += spent_time
-            # 해당 job의 대기+실행시간 누적
-            answer+=(now-start_time)
+            now += t
+            answer+= (now-req)
+            clear_job+=1
+            # print(f"heap : {heap}, start : {start}, now : {now}")
+
         else:
             now+=1
+            # print(f"heap : {heap}, start : {start}, now : {now}")
 
-        # print(f"start : {start}, now : {now}")
-
-    return math.trunc(answer/len(jobs))
+    return answer//len(jobs)
 
 
 if __name__ == "__main__":
