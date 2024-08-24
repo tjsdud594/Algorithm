977a310d64dc6169a5fa17b52986dd4fb1abcf58
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-08-24 09:22:34 +0900

    add : 뒤에 있는 큰 수 찾기 (heapq, stack)

---
+++ basic/programmers/FindBigBackNumber.py
@@ -0,0 +1,37 @@
+### 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+###############################
+#           풀이 중          #
+###############################
+
+from heapq import *
+
+def solution(numbers):
+    n = len(numbers)
+    answer = [-1] * n
+
+    h = []
+
+    for i in range(n):
+        value = numbers[i]
+
+        while h and h[0][0] < value:
+            _, idx = heappop(h)
+            answer[idx] = value
+
+        heappush(h, [value, i])
+    return answer
+
+
+
+
+if __name__ == "__main__":
+    import time
+    numbers = [2, 3, 3, 5]
+
+    print("start")
+    start = time.time()
+    print(solution(numbers))
+    end = time.time()
+
+    print(f"{end - start:.5f} sec")
